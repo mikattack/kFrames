16396e973701f41e9c1bc8ba0431684577670f39
Alex Mikitik <github@mikitik.com>
2015-08-23 10:52:50 -0400

    Add aura border and fix incorrect texture paths.
    Textures are not fonts.

--- config.lua
+++ config.lua
@@ -14,13 +14,14 @@ ns.elements = {}
 -----------------------------------------------------------------------------
 
 ns.media = {
-  largeFont = [[Interface\Addons\kFrames\media\font\BigNoodleTitling.ttf]],
-  smallFont = [[Interface\Addons\kFrames\media\font\Roboto-BoldCondensed.ttf]],
-  pixelFont = [[Interface\Addons\kFrames\media\font\Samson.ttf]],
-  primeBar  = [[Interface\Addons\kFrames\media\statusbar\Diagonal.ttf]],
-  statusBar = [[Interface\Addons\kFrames\media\statusbar\Resource.ttf]],
-  flatBar   = [[Interface\Addons\kFrames\media\statusbar\Flat.ttf]],
-  glowBar   = [[Interface\Addons\kFrames\media\statusbar\InsetGlow.ttf]],
+  largeFont   = [[Interface\Addons\kFrames\media\font\BigNoodleTitling.ttf]],
+  smallFont   = [[Interface\Addons\kFrames\media\font\Roboto-BoldCondensed.ttf]],
+  pixelFont   = [[Interface\Addons\kFrames\media\font\Samson.ttf]],
+  primeBar    = [[Interface\Addons\kFrames\media\statusbar\Diagonal.tga]],
+  statusBar   = [[Interface\Addons\kFrames\media\statusbar\Resource.tga]],
+  flatBar     = [[Interface\Addons\kFrames\media\statusbar\Flat.tga]],
+  glowBar     = [[Interface\Addons\kFrames\media\statusbar\InsetGlow.tga]],
+  auraBorder  = [[Interface\Addons\kFrames\media\border\Aura.tga]],
 }
 
 -----------------------------------------------------------------------------
