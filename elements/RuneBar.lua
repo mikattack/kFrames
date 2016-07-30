5b173e33e1ff7e2a0a730faf9e8a5b78f3070f98
Alex Mikitik <github@mikitik.com>
2016-07-30 14:16:22 -0700

    Add rune bar for Death Knights.

---
+++ elements/RuneBar.lua
@@ -0,0 +1,56 @@
+--[[--------------------------------------------------------------------
+  oUF_Kellen
+  Kellen's PVE-oriented layout for oUF.
+  Copyright (c) 2015-2016
+    Kellen <addons@mikitik.com>
+    All rights reserved.
+  https://github.com/mikattack/kFrames
+----------------------------------------------------------------------]]
+
+local _, ns = ...
+
+local playerClass = ns.util.playerClass
+local STATUSBAR = ns.media.statusBar or "Interface\\TargetingFrame\\UI-StatusBar"
+local r, g, b = 106/255, 184/255, 247/255
+
+
+function ns.elements.RuneBar(frame, position)
+  if playerClass ~= "DEATHKNIGHT" then return end
+
+  local frameWidth  = ns.config.classbarWidth
+  local runeWidth  = (frameWidth - 2 * 7) / 6
+  local runeHeight = 18
+  local p1, parent, p2, x, y = ns.util.parsePosition(position)
+
+  local runes = CreateFrame("Frame", nil, frame)
+  runes:SetPoint(p1, parent, p2, x, y)
+  runes:SetHeight(runeHeight)
+  runes:SetWidth(frameWidth)
+  runes.multiplier = 0.3
+
+  runes.background = runes:CreateTexture(nil, "BACKGROUND")
+  runes.background:SetAllPoints(runes)
+  runes.background:SetColorTexture(0, 0, 0, 0.5)
+
+  for i = 1, 6 do
+    local rune = CreateFrame("StatusBar", nil, frame)
+    rune:SetWidth(runeWidth)
+    rune:SetHeight(runeHeight - 4) -- 2 * 2px borders
+    rune:SetStatusBarTexture(STATUSBAR)
+    rune:SetStatusBarColor(r, g, b, 1)
+
+    rune.bg = rune:CreateTexture(nil, "BACKGROUND")
+    rune.bg:SetAllPoints(rune)
+    rune.bg:SetTexture(STATUSBAR)
+
+    if (i == 1) then
+      rune:SetPoint('LEFT', runes, 'LEFT', 2, 0)
+    else
+      rune:SetPoint('TOPLEFT', runes[i-1], 'TOPRIGHT', 2, 0)
+    end
+
+    runes[i] = rune
+  end
+  
+  frame.Runes = runes
+end
---
+++ layouts/deathknight.lua
@@ -0,0 +1,19 @@
+--[[--------------------------------------------------------------------
+  oUF_Kellen
+  Kellen's PVE-oriented layout for oUF.
+  Copyright (c) 2015-2016
+    Kellen <addons@mikitik.com>
+    All rights reserved.
+  https://github.com/mikattack/kFrames
+----------------------------------------------------------------------]]
+
+
+local _, ns = ...
+ns.layouts["DEATHKNIGHT"] = {}
+layout = ns.layouts["DEATHKNIGHT"]
+
+
+function layout.onCreate(frame)
+  -- Combo Point interface
+  ns.elements.RuneBar(frame, ns.position.classbars)
+end
