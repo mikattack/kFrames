9299b59f6fe4448783f14750e1def9d9ee7b9bea
Alex Mikitik <alex@mikitik.com>
2018-07-31 10:26:54 -0700

    Refactor initialization for improved clarity.

--- initialize.lua
+++ initialize.lua
@@ -1,37 +1,33 @@
---[[--------------------------------------------------------------------
-Initialize -  Set up all frames of the layout. Only loaded after
-              initializing everything else.
-----------------------------------------------------------------------]]
+-----------------------------------------------------------------------------
+-- Initialize -  Set up all frames for the layout. Only loaded after
+--               initializing everything else.
+-----------------------------------------------------------------------------
 
 
-local _, ns = ...
+local _, addon = ...
 
-local defaults = ns.defaults
-local elements = ns.elements
-local frames = ns.frames
-local position = defaults.position
+local defaults = addon.defaults
+local elements = addon.elements
+local frames   = addon.frames
 
-local sprint = ns.util.print
-local parsePosition = ns.util.parsePosition
+local sprint = addon.util.print
 
-ns.generated = {}
+-- Disable Blizzard frames
+oUF:DisableBlizzard('party')
 
 
 -----------------------------------------------------------------------------
 
 
 -- 
--- Define which frames we spawn via oUF and the functions we use to
--- decorate them.
+-- Define decorator functions for frames supported by the layout.
 -- 
-local supportedFrames = {
-  player        = frames.PlayerFrame,
-  target        = frames.TargetFrame,
+local decorators = {
+  player = frames.PlayerFrame,
+  target = frames.TargetFrame,
+  pet    = frames.PetFrame,
+  boss   = frames.Boss
   --targettarget  = frames.TargetOfTargetFrame,
-  pet           = frames.PetFrame,
-  --raid          = frames.RaiderFrame,  -- Someday maybe
-  --boss          = frames.BossFrame,
-  --maintank      = frames.MainTankFrame,
 }
 
 
@@ -39,75 +35,41 @@ local supportedFrames = {
 
 
 -- 
--- These "styles" define common setup code for groups of frames.
+-- Decoration function applied to each unit frame.
 -- 
-
-
--- 
--- Common unit frames
--- 
-local function UnitStyle(self, unit, isSingle)
-  if supportedFrames[unit] == nil then
-    sprint("Cannot create unsupported frame \[%s\]", unit)
-    return
-  end
-
-  self.menu = ns.util.rightClickMenu
+local function ApplyUnitStyle(self, unit, isSingle)
   self:RegisterForClicks("AnyUp")
-  self.RegisterForRoleChange = ns.util.registerForRoleChange
-
+  self.menu = addon.util.rightClickMenu
   self.unit = unit
-  ns.generated[unit] = supportedFrames[unit](self)
 
---[[
-  local p1, parent, p2, x, y = parsePosition(position[unit])
-  if parent ~= "UIParent" and ns.frames[parent] then
-    parent = ns.frames[parent]
-  elseif parent ~= "UIParent" then
-    sprint("Cannot attatch to unknown frame \[%s\]", parent)
-    parent = "UIParent"   -- Your position was The Wrong
+  fn = unit
+  if string.find(unit, "boss") then fn = "boss" end
+
+  if decorators[fn] then
+    decorators[fn](self)
   end
-  self:SetPoint(p1, parent, p2, x, y)
---]]
-end
-
-
--- 
--- Boss unit frames
--- 
-local function BossStyle(self, unit, isSingle)
-  baseunit = gsub(unit, "%d", "")
-  self.RegisterForRoleChange = ns.util.RegisterForRoleChange
-  self.unit = unit
-  ns.generated[unit] = supportedFrames[baseunit](self)
-end
-
-
--- 
--- Tank unit frames
--- 
-local function TankStyle(self, unit, isSingle)
-  baseunit = gsub(unit, "%d", "")
-  self.RegisterForRoleChange = ns.util.RegisterForRoleChange
-  self.unit = unit
-  ns.generated[unit] = supportedFrames[baseunit](self)
 end
 
 
 -----------------------------------------------------------------------------
 
 
-oUF:RegisterStyle("kUnit", UnitStyle)
---oUF:RegisterStyle("kBoss", BossStyle)
---oUF:RegisterStyle("kTank", TankStyle)
+--[[
+oUF How-To:
+  To spawn the unit frames for your layout, you must define a "style".
+  A style is essentially a named function that frame for each individual
+  unit will be passed to. Your function must decorate and position all
+  elements of that frame.
 
+  As there can be multiple styles, you must set which will be used by
+  the factory function. This is done with SetActiveStyle().
 
--- 
--- The oUF frames spawns the frames you specify just once. Any other
--- general frame-related setup should also go here (for example, the
--- prexisting MirrorTimers are modified here).
--- 
+  If you want to do any other frame-related setup, it's best to do it
+  in the factory function (example: decorating the MirrorTimers).
+--]]
+oUF:RegisterStyle("klnFrames", ApplyUnitStyle)
 oUF:Factory(function(self)
+  self:SetActiveStyle("klnFrames")
 
   -- Remove irrelevant rightclick menu entries
   for _, menu in pairs(UnitPopupMenus) do
@@ -119,29 +81,25 @@ oUF:Factory(function(self)
     end
   end
 
-  -- Units
-  self:SetActiveStyle("kUnit")
-  local units = {"player", "target", "pet"}
-  for i = 1, #units do
-    ns.generated[units[i]] = oUF:Spawn(units[i])
-  end
+  -- Main frames (decorators are assumed to position them)
+  oUF:Spawn("player")
+  oUF:Spawn("target")
+  oUF:Spawn("pet")
 
---[[
-  -- Boss
-  self:SetActiveStyle("kBoss")
-  local boss = {}
+  -- Boss frames
+  local boss_anchor = CreateFrame("frame", "klnFrames Boss Anchor", UIParent)
+  boss_anchor:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 15, -110)
+  boss_anchor.bosses = {}
   for i = 1, 5 do
     local b = oUF:Spawn("boss"..i, "oUF_Boss"..i)
+    b:SetParent(boss_anchor)
     if i == 1 then
-      local p1, parent, p2, x, y = parsePosition(position["boss"])
-      b:SetPoint(p1, parent, p2, x, y)
+      b:SetPoint("TOPLEFT", boss_anchor, "TOPLEFT", 0, 0)
     else
-      b:SetPoint("TOP", boss[i-1], "BOTTOM", 0, 5)
+      b:SetPoint("TOP", boss_anchor.bosses[i-1], "BOTTOM", 0, 5)
     end
-    boss[i] = b
-    ns.generated["boss"..i] = b
+    boss_anchor.bosses[i] = b
   end
---]]
 
   -- Decorate MirrorTimers
   for i = 1, 3 do
@@ -151,23 +109,16 @@ oUF:Factory(function(self)
 end)
 
 
--- Disable Blizzard frames
-oUF:DisableBlizzard('party')
-
-
--- When addon loads
-local function OnLoad(self, event, ...)
-  -- Create a false Target frame to show when no target is selected
-  --frames.FalseTargetFrame(defaults.width, defaults.height)
-
+-- 
+-- Perform additional setup when player first enters world.
+-- 
+local function OnLoad(self, event, ...),
   -- Disable Blizzard raid controls
   CompactRaidFrameManager:UnregisterAllEvents()
   CompactRaidFrameManager:Hide()
   CompactRaidFrameContainer:UnregisterAllEvents()
   CompactRaidFrameContainer:Hide() 
 end
-
-local addon = CreateFrame("Frame", nil, UIParent)
-addon:SetScript("OnEvent", OnLoad)
-addon:RegisterEvent("PLAYER_ENTERING_WORLD")
-  
+local onLoad = CreateFrame("Frame", nil, UIParent)
+onLoad:SetScript("OnEvent", OnLoad)
+onLoad:RegisterEvent("PLAYER_ENTERING_WORLD")
