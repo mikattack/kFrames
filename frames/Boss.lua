afbd348b67e3c5ce00177252fa9e381e7d857ff0
Alex Mikitik <alex@mikitik.com>
2018-07-31 10:31:31 -0700

    Add boss decorator, fix addon’s URL.

---
+++ frames/Boss.lua
@@ -0,0 +1,34 @@
+
+local _, addon = ...
+
+local defaults = addon.defaults
+local elements = addon.elements
+local frames   = addon.frames
+
+local PADDING = defaults.padding
+local FONT    = addon.media.font.small or STANDARD_TEXT_FONT
+
+
+function frames.BossFrame(frame)
+  local height = defaults.frames.minor.health_height
+  local width  = defaults.frames.minor.width
+
+  elements.InitializeUnitFrame(frame, {
+    fontsize = 34,
+    height   = height,
+    width    = width
+  })
+
+  -- Name
+  local name = elements.NewString(frame.Health, { font=FONT, size=22 })
+  name:SetPoint("LEFT", frame.Health, "LEFT", 5, -6)
+  frame:Tag(name, "[kFrames:name]")
+
+  -- Reposition health text
+  frame.HealthText:ClearAllPoints()
+  frame.HealthText:SetPoint("BOTTOMRIGHT", frame.Health, "BOTTOMRIGHT", -2, -10)
+
+  -- Niceties
+  elements.AddDispelHighlight(frame)
+  elements.AddHighlight(frame)
+end
